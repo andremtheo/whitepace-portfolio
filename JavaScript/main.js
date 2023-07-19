6f6fe12726669eadd87838a0900cffe5c9fa452a
andre <andre.mtheo4@gmail.com>
2023-07-19 11:27:56 -0300

    site Whitepace-Portfólio

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,8 +1,10 @@
-let ul = document.querySelector("nav.ul");
+
+let ul = document.querySelector("nav .ul");
 
 function openMenu(){
-    ul.classList.add("open")
+    ul.classList.add("open");
 }
+
 function closeMenu(){
-    ul.classList.remove("open")
+    ul.classList.remove("open");
 }
